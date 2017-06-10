d1923c53880903012fe48dab0361c7b3c7c2cac9
Johnny Reina <johnny.r.reina@gmail.com>
2017-06-10 15:23:53 -0500

    Creates project.

---
+++ src/cellular-automata/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cellular_automata.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/cellular-automata/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cellular_automata.MvcApplication" Language="C#" %>
