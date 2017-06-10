57cf7aa45bbc9065abe9699c6fa05f82eb96a49f
Johnny Reina <johnny.r.reina@gmail.com>
2017-06-10 17:46:31 -0500

    Cleans up appearance a bit.

--- src/cellular-automata/Scripts/app.js
+++ src/cellular-automata/Scripts/app.js
@@ -8,10 +8,7 @@
         let width = Number.parseInt(form.width.value)
         let generations = Number.parseInt(form.generations.value)
 
-        console.log(`on: ${onChar}, off: ${offChar}, rule: ${ruleNum}, width: ${width}, generations: ${generations}`)
-
         const grid = new CAGrid(width, generations, ruleNum)
-        console.log(grid.Rule)
 
         $('#sim-table').html(grid.GridText(offChar, onChar))
     })
