2a58ce5b31c3fe7fc67b024d24d0cc376f7ea78e
Johnny Reina <johnny.r.reina@gmail.com>
2017-06-12 08:13:17 -0500

    Adds ranges to initial conditions. Adds opengraph URL and Image.

--- src/cellular-automata/Scripts/CAGrid.js
+++ src/cellular-automata/Scripts/CAGrid.js
@@ -1,20 +1,28 @@
 ﻿// Construct a RowManager instance with the specified number of cells.
-function CAGrid(width, height, rule, intial) {
-  
+function CAGrid(width, height, rule, intial, off, on) {
+
   const grid = [Array(width).fill(0)]
   const ca = new CellularAutomata(rule)
+  let offChar = off || '#';
+  let onChar = on || '_';
+  let gridString = '';
+  const initial = Array(width).fill(0)
+  intial.forEach(val => { if (val > -1 && val < width) initial[val] = 1 })
+  let currentRow = initial
 
-  intial.forEach(val => { if (val > -1 && val < width) grid[0][val] = 1 })
   for (let i = 1; i < height; i++) {
-      grid.push(ca.EvolveRow(grid[i - 1]))
+    let newRow = ca.EvolveRow(currentRow)
+    gridString += newRow.map(cell => cell === 1 ? onChar : offChar).join('') + '<br />'
+    currentRow = newRow
   }
   this.Grid = function () {
     return grid
   }
   this.Rule = ca.RuleSet()
-  this.GridText = function(off, on) {
-      let offChar = off || '#';
-      let onChar = on || '_';
-      return grid.reduce((rowMemo, row) => rowMemo + row.map(cell => cell === 1 ? onChar : offChar).join('') + '<br />', '')
+  this.GridText = function (off, on) {
+    return grid.reduce((rowMemo, row) => rowMemo + row.map(cell => cell === 1 ? onChar : offChar).join('') + '<br />', '')
+  }
+  this.GridString = function () {
+    return gridString
   }
 }
--- src/cellular-automata/Scripts/app.js
+++ src/cellular-automata/Scripts/app.js
@@ -10,11 +10,18 @@
         let ruleNum = Number.parseInt(form.rule.value)
         let width = Number.parseInt(form.width.value)
         let generations = Number.parseInt(form.generations.value)
-        let initial = form.initial.value.split(',')
+        let initial = form.initial.value.split(',').reduce((memo, val) => {
+          if (val.indexOf('..') !== -1) {
+            let params = val.split('..').map(param => Number.parseInt(param))
+            if (params.length !== 2) throw new Error('Syntax error near ' + val)
+            for (let i = params[0]; i <= params[1]; i++) memo.push(i)
+          } else memo.push(Number.parseInt(val))
+          return memo
+        }, [])
 
-        const grid = new CAGrid(width, generations, ruleNum, initial)
+        const grid = new CAGrid(width, generations, ruleNum, initial, offChar, onChar)
 
-        $('#sim-table').html(grid.GridText(offChar, onChar))
+        $('#sim-table').html(grid.GridString)
         $('.rule-row').remove()
         Object.keys(grid.Rule).forEach(ruleKey => {
             $('#rules-table').append(
