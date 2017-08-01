5607ba207c1deac698d9fdd7e3e8d3c76ddf0a71
Johnny Reina <johnny.r.reina@gmail.com>
2017-07-31 21:14:45 -0500

    Adds static html page.

---
+++ docs/js/cellular-automata-utils.js
@@ -0,0 +1,18 @@
+﻿// Some utilities for this project.
+
+const Utils = {
+  // Convert an integer to a binary represented by an array.
+  ConvertIntToBinArray: function convertToBinArray(ar, remainder, index) {
+    if (index === ar.length) return ar
+    let powerOfTwo = Math.pow(2, ar.length - index - 1)
+    ar[index] = remainder >= powerOfTwo ? 1 : 0
+    return convertToBinArray(ar, remainder >= powerOfTwo ? remainder - powerOfTwo : remainder, index + 1)
+  },
+
+  // Return a copy of the object
+  Copy: function (obj) {
+    return Object
+      .keys(obj)
+      .reduce((memo, key) => { memo[key] = obj[key]; return memo }, {})
+  }
+}
---
+++ src/CellularAutomata/CellularAutomata.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CellularAutomata.Utilities;
+
+namespace CellularAutomata
+{
+    public class CellularAutomata
+    {
+        int _rule;
+        List<int> _ruleBinList;
+        Dictionary<string, bool> _rules;
+
+        public CellularAutomata(int rule)
+        {
+            if (rule < 0 || rule > 255) throw new Exception("Rule must be in the range [0, 255]");
+
+            _rule = rule;
+            _ruleBinList = BinaryUtilities.ToBinArray(rule, 8);
+
+            var cellPermutations = Enumerable.Range(0, 7).Select(index => BinaryUtilities.ToBinArray(index, 3)).ToList();
+            for (var rulePermutationIndex = 0; rulePermutationIndex < cellPermutations.Count; rulePermutationIndex++)
+            {
+                _rules[string.Join(string.Empty, cellPermutations[rulePermutationIndex])] = _ruleBinList[_ruleBinList.Count - rulePermutationIndex - 1] === 1;
+            }
+
+        }
+
+        public bool Check(List<int> ar)
+        {
+            if (ar.Count != 3) throw new Exception("List must have three indices to be checked.");
+            return _rules[string.Join(string.Empty, ar)];
+        }
+
+        public List<int> EvolveRow(List<int> row) {
+            return row.Select()
+        }
+    }
+}
